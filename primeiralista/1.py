86cf9d9feec2bd69c6b96d7fe9731ba55d283d4a
dansilx <danilosilveira@msn.com>
2023-07-01 16:55:18 -0300

    Enviando minha primeira lista

---
+++ primeiralista/1.py
@@ -0,0 +1,6 @@
+# 1º passo - identificar a(s) entrada(s) - variável número
+numero = int(input('Informe um valor: '))
+# 2º passo - identificar o processamento/cálculo
+resultado = numero * 2
+# 3º passo - apresentar a(s) variáveis que foram calculadas
+print('O resultado é',resultado)
---
+++ primeiralista/2.py
@@ -0,0 +1,8 @@
+# 1° passo - entrada de dados/valores
+numero1 = int(input('Cadastre o primeiro número: '))
+numero2 = int(input('Cadastre o segundo número: '))
+numero3 = int(input('Cadastre o terceiro número: '))
+# 2º passo - processamento/cálculo
+resultado = numero1 + numero2 + numero3
+# 3° passo - saída/apresentação de resultado
+print('A soma dos número é',resultado)
