cedb35cd7134262062f1c331532268a0f999093a
dansilx <danilosilveira@msn.com>
2023-07-01 17:17:21 -0300

    fix test

--- primeiralista/2.py
+++ primeiralista/2.py
@@ -5,4 +5,4 @@ numero3 = int(input('Cadastre o terceiro número: '))
 # 2º passo - processamento/cálculo
 resultado = numero1 + numero2 + numero3
 # 3° passo - saída/apresentação de resultado
-print('A soma dos número é',resultado)
+print('A soma dos número é...',resultado)
